0ae80dceaca31418eb8515f8e3207075dcfe795e
undividedhawk <bradleywilliammueller@gmail.com>
2017-09-27 11:39:39 -0500

    testing nearest

--- resize/resample.py
+++ resize/resample.py
@@ -56,38 +56,38 @@ class resample:
 
 
 
-    def bilinear_interpolation(self, image, fx, fy):
-        """resizes an image using bilinear interpolation approximation for resampling
-        image: the image to be resampled
-        fx: scale along x direction (eg. 0.5, 1.5, 2.5)
-        fx: scale along y direction (eg. 0.5, 1.5, 2.5)
-        returns a resized image based on the bilinear interpolation method
-        """
-
-        # Write your code for bilinear interpolation here
-        (width, height) = image.shape
-        width = int(width)
-        height = int(height)
-        fx = float(fx)
-        fy = float(fy)
-        newWid = round(width * fx)
-        newHt = round(height * fy)
-        image2 = np.zeros((newWid, newHt), np.uint8)
-        image3 = np.resize(image2,(width,height))
-        for x in range(newWid):
-            for y in range(newHt):
-                z00 = image[x][y]
-                z01 = image[x][y+2]
-                z10 = image[x+2][y]
-                z11 = image[x+2][y+2]
-                r0 = (((y+2 - y+1) / (y+2 - y))* z10) + (((y+1 - y) / (y+2 - y))*z11)
-                c0 = (((y+2 - y+1) / (y+2 - y))* z00) + (((y+1 - y) / (y+2 - y))*z01)
-                image3[x][y] = (r0*((x - x+1)/(x - x+2))) + (c0*((x+1 - x+2)/(x - x+2)))
-                if image3[x][y] > 255:
-                    image3[x][y] =
-        image2 = np.resize(image3, (newWid,newHt))
-
-        return image2
+    # def bilinear_interpolation(self, image, fx, fy):
+    #     """resizes an image using bilinear interpolation approximation for resampling
+    #     image: the image to be resampled
+    #     fx: scale along x direction (eg. 0.5, 1.5, 2.5)
+    #     fx: scale along y direction (eg. 0.5, 1.5, 2.5)
+    #     returns a resized image based on the bilinear interpolation method
+    #     """
+    #
+    #     # Write your code for bilinear interpolation here
+    #     (width, height) = image.shape
+    #     width = int(width)
+    #     height = int(height)
+    #     fx = float(fx)
+    #     fy = float(fy)
+    #     newWid = round(width * fx)
+    #     newHt = round(height * fy)
+    #     image2 = np.zeros((newWid, newHt), np.uint8)
+    #     image3 = np.resize(image2,(width,height))
+    #     for x in range(newWid):
+    #         for y in range(newHt):
+    #             z00 = image[x][y]
+    #             z01 = image[x][y+2]
+    #             z10 = image[x+2][y]
+    #             z11 = image[x+2][y+2]
+    #             r0 = (((y+2 - y+1) / (y+2 - y))* z10) + (((y+1 - y) / (y+2 - y))*z11)
+    #             c0 = (((y+2 - y+1) / (y+2 - y))* z00) + (((y+1 - y) / (y+2 - y))*z01)
+    #             image3[x][y] = (r0*((x - x+1)/(x - x+2))) + (c0*((x+1 - x+2)/(x - x+2)))
+    #             if image3[x][y] > 255:
+    #
+    #     image2 = np.resize(image3, (newWid,newHt))
+    #
+    #     return image2
 
 
 
